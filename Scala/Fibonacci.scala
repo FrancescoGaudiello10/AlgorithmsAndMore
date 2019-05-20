0a3af33f28236c4cf15c76e0c09080858c85905f
FrancescoGaudiello10 <francesco.gaudiello.1995@gmail.com>
2019-05-20 19:35:48 +0200

    fibo

--- Scala/Fibonacci.scala
+++ Scala/Fibonacci.scala
@@ -1,16 +1,16 @@
-/* 
-Scrivere un metodo Scala che calcola l’n-esimo numero 
-di Fibonacci F(n), dove F(1)=1, F(2)=1, F(3)=2, F(4)=3, F(5)=5
-*/
+/*  Fibonacci Algorithm */
 
-object E4 extends App{
+object Fibonacci extends App{
     def fibo(x:Int):Int = {
         x match {
             case 0 if(x == 0) => 0
             case 1 if(x == 1) => 1
-            case n => fibo(x-1)+fibo(x-2)
+            case n => fibo(x-1)+fibo(x-2) //Classic 
         }
     }
+
+
+    //Prove:
     println(fibo(0))
     println(fibo(1))
     println(fibo(2))
